32c2be7e4d6a149fd56573444d8870fe71c6da34
Lee Hyeon Jeong <hyeont0r1@gmail.com>
2018-05-25 20:25:45 +0900

    My first cal

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫번쨰 수를 입력하세요");
+        int a = sc.nextInt();
+
+        System.out.println("+,-,*,/,%를 입력하세요");
+        String mark = sc.next();
+
+        System.out.println("두번째 수를 입력하세요");
+        int b = sc.nextInt();
+
+        if (mark.equals("+")){
+            int result = a+b;
+            System.out.println(result);
+        }
+    }
+}
